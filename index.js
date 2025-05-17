6a79f3e2e4f13655ec6e7d8e807e1def4e59e16f
circlefour <0100.circle@gmail.com>
2025-05-17 19:01:29 -0400

    testing socket connection

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-console.log('hiiiii');
+//console.log('hiiiii');
+const socket = io("https://chaos-server-dev.up.railway.app/");
 
 function setupDeviceMotionListener() {
   window.addEventListener("devicemotion", (event) => {
-    console.log('hi wussup');
+    //console.log('hi wussup');
 
     const acceleration = event.accelerationIncludingGravity;
     const intensity = Math.sqrt(
       acceleration.x ** 2 + acceleration.y ** 2 + acceleration.z ** 2
     );
 
-    console.log("shake intensity: ", intensity);
+    //console.log("shake intensity: ", intensity);
     document.getElementById("txt").textContent = `${intensity}`;
+
+    socket.emit('shake', intensity);
   });
 }
 
